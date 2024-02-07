363b63c4b341126af143aa97722ba0652d6f2a86
maujogue <maujogue15@gmail.com>
2024-02-07 15:23:25 +0100

    mysite -> pong_app

--- pong_app/entrypoint.prod.sh
+++ pong_app/entrypoint.prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn mysite.wsgi:application --bind 0.0.0.0:8000
+gunicorn pong_app.wsgi:application --bind 0.0.0.0:8000

37a8f0bba33198821cc70c16c2719f4d9ce06773
boulatrousse <lboulatrous@gmail.com>
2024-02-23 17:59:43 +0100

    add password tests for register

--- pong_app/users/tests/test_views.py
+++ pong_app/users/tests/test_views.py
@@ -29,7 +29,7 @@ class SignUpTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(CustomUser.objects.count(), initial_user_count + 1)
 
-    def test_email_is_already_used(self):
+    def test_register_email_is_already_used(self):
         newUser = {
             'username': 'bob_seger',
             'email': 'lboulatr@gmail.com',
@@ -44,12 +44,42 @@ class SignUpTest(TestCase):
 
         self.assertEqual(response.status_code, 400)
 
-    def test_username_is_already_used(self):
+    def test_register_username_is_already_used(self):
         newUser = {
             'username': 'lboulatr',
             'email': 'routine@gmail.com',
             'password1': 'Newtrans9+',
             'password2': 'Newtrans9+'
+        }
+
+        response = self.client.post(
+            reverse('register'), 
+            data=json.dumps(newUser), 
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_register_weak_password(self):
+        newUser = {
+            'username': 'ochoa',
+            'email': 'ochoaloco@gmail.com',
+            'password1': '+',
+            'password2': '+'
+        }
+
+        response = self.client.post(
+            reverse('register'), 
+            data=json.dumps(newUser), 
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_register_different_password(self):
+        newUser = {
+            'username': 'ochoa',
+            'email': 'ochoaloco@gmail.com',
+            'password1': 'Km4C47_x£6v,',
+            'password2': 'Km4C47_x£6v,+'
         }
 
         response = self.client.post(

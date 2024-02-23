601623843086b16ad30816e68ff3c2da338a19eb
boulatrousse <lboulatrous@gmail.com>
2024-02-23 17:53:27 +0100

    add the 3 first unit tests for the register view

--- pong_app/users/tests/test_views.py
+++ pong_app/users/tests/test_views.py
@@ -5,11 +5,18 @@ import json
 
 class SignUpTest(TestCase):
 
+    def setUp(self):
+        CustomUser.objects.create(
+            username="lboulatr",
+            email="lboulatr@gmail.com",
+            password="Damiendubocal98")
+    
     def test_register(self):
         newUser = {
             'username': 'bob_seger',
             'email': 'bobseger@gmail.com',
-            'password': 'Newtrans9+'
+            'password1': 'Newtrans9+',
+            'password2': 'Newtrans9+'
         }
 
         initial_user_count = CustomUser.objects.count()
@@ -20,5 +27,34 @@ class SignUpTest(TestCase):
             content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
-        # self.assertRedirects(response, reverse('register_success'))
-        # self.assertEqual(CustomUser.objects.count(), initial_user_count + 1)
+        self.assertEqual(CustomUser.objects.count(), initial_user_count + 1)
+
+    def test_email_is_already_used(self):
+        newUser = {
+            'username': 'bob_seger',
+            'email': 'lboulatr@gmail.com',
+            'password1': 'Newtrans9+',
+            'password2': 'Newtrans9+'
+        }
+
+        response = self.client.post(
+            reverse('register'), 
+            data=json.dumps(newUser), 
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_username_is_already_used(self):
+        newUser = {
+            'username': 'lboulatr',
+            'email': 'routine@gmail.com',
+            'password1': 'Newtrans9+',
+            'password2': 'Newtrans9+'
+        }
+
+        response = self.client.post(
+            reverse('register'), 
+            data=json.dumps(newUser), 
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)

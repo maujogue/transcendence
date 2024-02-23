92fbb61043abd4ca8be6131440c8bc1da2fea37a
boulatrousse <lboulatrous@gmail.com>
2024-02-23 14:16:56 +0100

    rename  folder into  | fix login and register html | creation of tournament html

--- pong_app/users/urls.py
+++ pong_app/users/urls.py
@@ -9,5 +9,6 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
+	path('tournament/', views.tournament, name='tournament'),
     path('get_csrf_token/', views.get_csrf_token, name='get_csrf_token')
 ]

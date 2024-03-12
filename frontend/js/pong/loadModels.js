ed0c985fc5a4d1e1309a7a5ef6fab4f64c3f385f
Clement Gerardin <clementgerardin.cg@gmail.com>
2024-03-12 11:22:07 +0100

    add tmp icon

--- frontend/js/pong/loadModels.js
+++ frontend/js/pong/loadModels.js
@@ -2,17 +2,23 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { Character } from "./Class/Character.js";
 import { charactersNames } from "./varGlobal.js";
 import { characters } from './main.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+
 
 async function loadModel(fileName) {
+    console.log(fileName);
     return new Promise((resolve, reject) => {
         const loader = new GLTFLoader();
+        const draco = new DRACOLoader();
+        draco.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.4.3/');
+        loader.setDRACOLoader(draco);
 
         loader.load('assets/models/characters/' + fileName + '.glb',
             (gltf) => {
                 resolve(gltf);
             },
             (xhr) => {
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+                // console.log((xhr.loaded / xhr.total * 100) + '% loaded');
             },
             (error) => {
                 console.error('An error happened', error);
--- frontend/js/pong/varGlobal.js
+++ frontend/js/pong/varGlobal.js
@@ -1,3 +1,4 @@
 export const winWidth = 960;
 export const winHeight = 540;
-export const charactersNames = ['chupacabra', 'elvis'];
+export const charactersNames = ['chupacabra', 'elvis', 'granny', 'theBoss'];
+export const colors = ['ffffff', 'ff0000', '0000ff', '00ff00', 'ffff00', 'ff00ff', '00ffff', 'ff8800'];

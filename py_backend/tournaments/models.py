445a34ccf07955563201e8a0200877a00321697d
LezardSC <Lezardsouscultive@gmail.com>
2024-03-18 18:36:45 +0100

    idk what i have to do

--- py_backend/tournaments/models.py
+++ py_backend/tournaments/models.py
@@ -7,9 +7,15 @@ class Tournament(models.Model):
 	
 	name = models.fields.CharField(max_length=100)
 	# host = models.ForeignKey(CustomUser, null=True, on_delete=models.SET_NULL)
+	password = models.fields.CharField(max_length=100)
 	n_players = models.IntegerField(validators=[MinValueValidator(2), MaxValueValidator(32)])
-	start_date = models.DateTimeField()
-	end_date = models.DateTimeField()
 
 	def __str__(self):
 		return f'{self.name}'
+	
+"""
+-name of the tournament
+-number players max
+-private/public
+-password if private
+"""

dae051a6cba58ce7428071dfdba35a09e827c410
gclement42 <clement.games125@gmail.com>
2024-04-11 09:00:10 +0200

    create interface for join Tournament

--- frontend/js/pong/menu.js
+++ frontend/js/pong/menu.js
@@ -45,10 +45,6 @@ function createSwatchPanel(leftPos, nb, character) {
 
 	newDiv.style.zIndex = "100";
 	newDiv.style.display = "block";
-	// newDiv.style.left = leftPos + "px";
-	// newDiv.style.bottom = "0px";
-	// newDiv.style.width = ((width / 2) / charactersNames.length) + "px";
-	// newDiv.style.border = "1px solid black";
 	newDiv.style.height = "50%";
 	document.getElementById("panel").appendChild(newDiv);
 	createIcon(newDiv, character);
@@ -285,31 +281,42 @@ function createFormTournament(parent) {
 	parent.innerHTML = '\
 	<i class="fa-solid fa-arrow-left icon" id="backIcon"></i>\
 	<div id="createTournamentForm">\
+	<h1 class="form-header glitched">Create Tournament</h1>\
 	<form id="tournamentForm" method="post">\
 	<div class="form-field">\
-		<label for="nom">Name :</label>\
-		<input type="text" id="nom" name="nom" required>\
+		<label for="nom" class="form-field-name glitched">Name :</label>\
+		<input class="glitched" type="text" id="nom" name="nom" required>\
 	</div>\
 	<div class="form-field">\
-		<label for="nb_joueurs_max">Max players :</label>\
-		<input type="number" id="nb_joueurs_max" name="nb_joueurs_max" min="2" required>\
+		<label for="max_players" class="form-field-max_players glitched">Max players :</label>\
+		<input type="number" class="glitched" id="max_players" name="max_players" min="2" required>\
 	</div>\
 	<div class="form-field">\
-		<label for="points_par_match">Max points :</label>\
-		<input type="number" id="points_par_match" name="points_par_match" min="1" required>\
+		<label for="points_par_match" class="glitched form-field-max_points">Max points :</label>\
+		<input class="glitched" type="number" id="points_par_match" name="points_par_match" min="1" required>\
 	</div>\
-	<div class="form-field">\
-		<label for="prive">Private :</label>\
-		<input type="checkbox" id="prive" name="prive">\
+	<div class="form-field form-field__private">\
+		<label for="prive" class="glitched">Private :</label>\
+		<input type="checkbox" id="private" class="glitched" name="prive">\
 	</div>\
-	<div style="display: none;" class="form-field">\
-		<label for="mot_de_passe">Password :</label>\
-		<input type="password" id="mot_de_passe" name="mot_de_passe">\
+	<div class="form-field form-field__password disabled">\
+		<label for="tournamentPassword" class="glitched" >Password :</label>\
+		<input type="password" class="glitched" id="tournamentPassword" name="tournamentPassword">\
 	</div>\
-	<div>\
-		<button class="btn-create icon" type="submit">Create</button>\
-	</div></div>\
+	<div class="glitched">\
+		<button class="btn-create" type="submit">Create</button>\
+	</div>\
+		</div>\
 	</form>'
+
+	const privateCheckbox = document.getElementById("private");
+	const passwordField = document.querySelector(".form-field__password");
+	privateCheckbox.addEventListener("change", (e) => {
+		if (e.target.checked)
+			passwordField.classList.remove("disabled");
+		else
+			passwordField.classList.add("disabled");
+	});
 }
 
 function createTournamentInfo(name, nb) {

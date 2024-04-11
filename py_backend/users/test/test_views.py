a6177004b1a086d7606f158e21ebf0036428456a
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-04-11 11:22:51 +0200

    add test_post_without_csrf_token_with_csrf_checks

--- py_backend/users/test/test_views.py
+++ py_backend/users/test/test_views.py
@@ -507,7 +507,6 @@ class LogoutTests(TestCase):
 class CSRFTokenTest(TestCase):
     def setUp(self):
         self.csrf_client = Client(enforce_csrf_checks=True)
-
         self.user = CustomUser.objects.create_user(
             username="osterga",
             email="osterga@gmail.com",
@@ -516,6 +515,7 @@ class CSRFTokenTest(TestCase):
         self.client.login(username='osterga', password='UserPassword9+')
 
     def test_get_csrf_token(self):
+    
         response = self.client.get(reverse('get_csrf_token'))
 
         self.assertEqual(response.status_code, 200)
@@ -523,30 +523,42 @@ class CSRFTokenTest(TestCase):
         self.assertIn('csrftoken', response.cookies)
     
     def test_post_response(self):
+    
         response = self.client.post(reverse('get_csrf_token'))
         self.assertEqual(response.status_code, 405)
 
     def test_get_request_for_post_function(self):
+    
         response = self.client.get(reverse('register'))
         self.assertEqual(response.status_code, 405)
 
     def test_csrf_token_is_unique(self):
+    
         response1 = self.client.get(reverse('get_csrf_token'))
         response2 = self.client.get(reverse('get_csrf_token'))
         self.assertNotEqual(response1.json()['csrfToken'], response2.json()['csrfToken'])
         self.assertEqual(response1.status_code, 200)
         self.assertEqual(response2.status_code, 200)
 
-    def test_token(self):
+    def test_token_logout_with_token(self):
         response = self.client.get(reverse('get_csrf_token'))
 
         response = self.client.post(
             reverse('logout_view'), 
             content_type='application/json')
 
-        # Assert the expected outcome
         self.assertEqual(response.status_code, 200)
 
+    def test_token_logout_without_token(self):
+        response = self.client.post(
+            reverse('logout_view'), 
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_without_csrf_token_with_csrf_checks(self):
+        response = self.csrf_client.post(reverse('update_profile'), data={'key': 'value'})
+        self.assertEqual(response.status_code, 403)
 
 # # =========================================================================================
         

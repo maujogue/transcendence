79f101bde2bba897dd3a6e38b440486af06c745c
Mathis <maujogue15@gmail.com>
2024-04-17 13:51:03 +0200

    added get_csrf_token function for cleaner code

--- frontend/js/ApiCalls.js
+++ frontend/js/ApiCalls.js
@@ -1,7 +1,19 @@
 import { navigateTo } from "./Router.js";
 import { showComment } from "./Utils.js";
 
-function register(event, registerForm) {
+function get_csrf_token() {
+  return fetch("https://127.0.0.1:8000/api/users/get_csrf_token/", {
+    method: "GET",
+    credentials: "include",
+  })
+    .then((response) => response.json())
+    .then((data) => data.csrfToken)
+    .catch((error) => {
+      console.error("get csrf token fail", error);
+    });
+}
+
+async function register(event, registerForm) {
   event.preventDefault();
 
   const userData = new FormData(registerForm);
@@ -12,46 +24,35 @@ function register(event, registerForm) {
     password2: userData.get("passwordagain"),
   };
 
-  fetch("https://127.0.0.1:8000/api/users/get_csrf_token/", {
-    method: "GET",
+  fetch("https://127.0.0.1:8000/api/users/register/", {
+    method: "POST",
+    headers: {
+      "X-CSRFToken": await get_csrf_token(),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
     credentials: "include",
+    body: JSON.stringify(fetchBody),
   })
     .then((response) => response.json())
     .then((data) => {
-      const csrfToken = data.csrfToken;
-      fetch("https://127.0.0.1:8000/api/users/register/", {
-        method: "POST",
-        headers: {
-          "X-CSRFToken": csrfToken,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        credentials: "include",
-        body: JSON.stringify(fetchBody),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.status === "success") {
-            showComment(
-              "registerComment",
-              "Registered, you can now Login",
-              "success"
-            );
-          } else {
-            if (data.error && data.error.length > 0)
-              showComment("registerComment", data.error[0]);
-            else showError("registerComment", "Register Error");
-          }
-        })
-        .catch((error) => {
-          console.error("register failed", error);
-        });
+      if (data.status === "success") {
+        showComment(
+          "registerComment",
+          "Registered, you can now Login",
+          "success"
+        );
+      } else {
+        if (data.error && data.error.length > 0)
+          showComment("registerComment", data.error[0]);
+        else showError("registerComment", "Register Error");
+      }
     })
     .catch((error) => {
-      console.error("get csrf token fail", error);
+      console.error("register failed", error);
     });
 }
 
-function login(event, loginForm) {
+async function login(event, loginForm) {
   event.preventDefault();
 
   const userData = new FormData(loginForm);
@@ -60,114 +61,76 @@ function login(event, loginForm) {
     password: userData.get("password"),
   };
 
-  fetch("https://127.0.0.1:8000/api/users/get_csrf_token/", {
-    method: "GET",
+  fetch("https://127.0.0.1:8000/api/users/login/", {
+    method: "POST",
+    headers: {
+      "X-CSRFToken": await get_csrf_token(),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    credentials: "include",
+    body: JSON.stringify(fetchBody),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "success") {
+        Cookies.set("isLoggedIn", "true");
+        bootstrap.Modal.getInstance(document.getElementById("login")).hide();
+        navigateTo("/dash");
+      } else {
+        showError("loginError", "Username or Password incorrect");
+      }
+    })
+    .catch((error) => {
+      console.error("login failed", error);
+    });
+}
+
+async function logout() {
+  fetch("https://127.0.0.1:8000/api/users/logout/", {
+    method: "POST",
+    headers: {
+      "X-CSRFToken": await get_csrf_token(),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
     credentials: "include",
   })
     .then((response) => response.json())
     .then((data) => {
-      const csrfToken = data.csrfToken;
-      fetch("https://127.0.0.1:8000/api/users/login/", {
-        method: "POST",
-        headers: {
-          "X-CSRFToken": csrfToken,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        credentials: "include",
-        body: JSON.stringify(fetchBody),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.status === "success") {
-            Cookies.set("isLoggedIn", "true");
-            bootstrap.Modal.getInstance(
-              document.getElementById("login")
-            ).hide();
-            navigateTo("/dash");
-          } else {
-            showError("loginError", "Username or Password incorrect");
-          }
-        })
-        .catch((error) => {
-          console.error("login failed", error);
-        });
+      if (data.status === "success") {
+        Cookies.remove("isLoggedIn");
+        navigateTo("/dash");
+      }
     })
     .catch((error) => {
-      console.error("get csrf token fail", error);
+      console.error("logout failed", error);
     });
 }
 
-function logout() {
-  fetch("https://127.0.0.1:8000/api/users/get_csrf_token/", {
-    method: "GET",
-    credentials: "include",
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const csrfToken = data.csrfToken;
-      fetch("https://127.0.0.1:8000/api/users/logout/", {
-        method: "POST",
-        headers: {
-          "X-CSRFToken": csrfToken,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        credentials: "include",
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.status === "success") {
-            Cookies.remove("isLoggedIn");
-            navigateTo("/dash");
-          }
-        })
-        .catch((error) => {
-          console.error("logout failed", error);
-        });
-    })
-    .catch((error) => {
-      console.error("get csrf token fail", error);
-    });
-}
-
-function updateProfile(event, updateProfileForm) {
+async function updateProfile(event, updateProfileForm) {
   event.preventDefault();
 
   const userData = new FormData(updateProfileForm);
   const fetchBody = {
     username: userData.get("username"),
     bio: userData.get("bio"),
-    //  avatar: userData.get("avatar"),
+    avatar: userData.get("avatar"),
   };
-
-  console.log(userData);
-  fetch("https://127.0.0.1:8000/api/users/get_csrf_token/", {
-    method: "GET",
+  fetch("https://127.0.0.1:8000/api/users/update_profile/", {
+    method: "POST",
+    headers: {
+      "X-CSRFToken": await get_csrf_token(),
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
     credentials: "include",
+    body: JSON.stringify(fetchBody),
   })
     .then((response) => response.json())
     .then((data) => {
-      const csrfToken = data.csrfToken;
-      console.log(csrfToken);
-      fetch("https://127.0.0.1:8000/api/users/update_profile/", {
-        method: "POST",
-        headers: {
-          "X-CSRFToken": csrfToken,
-          Accept: "application/json",
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        credentials: "include",
-        body: JSON.stringify(fetchBody),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => {
-          console.error("login failed", error);
-        });
+      console.log(data);
     })
     .catch((error) => {
-      console.error("get csrf token fail", error);
+      console.error("login failed", error);
     });
 }
 

254827ca274a7e28d68f8ee7048248f94ce8492a
Clement Gerardin <77669674+gclement42@users.noreply.github.com>
2024-04-23 12:47:36 +0200

    Update createTournament.js

--- frontend/js/pong/createTournament.js
+++ frontend/js/pong/createTournament.js
@@ -1,46 +1,49 @@
 export async function sendTournamentForm(form) {
     console.log("sendTournamentForm")
     createWaitingScreenTournament();
-    // const formData = new FormData(form);
-    // const fetchBody = {
-    //     name: formData.get("name"),
-    //     max_players: formData.get("max_players"),
-    //     is_private: false,
-    //     password: formData.get("tournamentPassword"),
-    // }
-    // console.log('Envoi du formulaire :', JSON.stringify(fetchBody));
+}
 
-    // fetch("https://127.0.0.1:8000/api/get_csrf_token/", {
-	// 	method: "GET",
-	// 	credentials: "include",
-	// })
-	// .then((response) => response.json())
-	// .then((data) => {
-    //     fetch('https://127.0.0.1:8000/api/create_tournament/', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/x-www-form-urlencoded',
-    //             "X-CSRFToken": data.csrfToken,
-    //         },
-    //         credentials: "include",
-    //         body: JSON.stringify(fetchBody),
-    //     })
-    //     .then((response) => response.json())
-	// 	.then((data) => {
-	// 		if (data.error) {
-	// 			alert(data.error);
-	// 		}
-	// 		else {
-	// 			alert("Tournament created");
-	// 		}
-	// 	})
-	// 	.catch((error) => {
-	// 		console.error("create Tournament", error);
-	// 	});
-    // })
-    // .catch((error) => {
-    //     console.error("get csrf token fail", error);
-    // });
+function fetchCreateTournament(formData){
+	const formData = new FormData(form);
+	const fetchBody = {
+	name: formData.get("name"),
+	max_players: formData.get("max_players"),
+	is_private: false,
+	password: formData.get("tournamentPassword"),
+	}
+	console.log('Envoi du formulaire :', JSON.stringify(fetchBody));
+	
+	fetch("https://127.0.0.1:8000/api/get_csrf_token/", {
+		method: "GET",
+		credentials: "include",
+	})
+	.then((response) => response.json())
+	.then((data) => {
+	fetch('https://127.0.0.1:8000/api/create_tournament/', {
+	    method: 'POST',
+	    headers: {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		"X-CSRFToken": data.csrfToken,
+	    },
+	    credentials: "include",
+	    body: JSON.stringify(fetchBody),
+	})
+	.then((response) => response.json())
+		.then((data) => {
+			if (data.error) {
+				alert(data.error);
+			}
+			else {
+				alert("Tournament created");
+			}
+		})
+		.catch((error) => {
+			console.error("create Tournament", error);
+		});
+	})
+	.catch((error) => {
+	console.error("get csrf token fail", error);
+	});
 }
 
 export function createWaitingScreenTournament() {

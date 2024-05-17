4fd28154b8e345658c11bbdd8e1381839856a7a7
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-05-17 15:13:45 +0200

    stash

--- py_backend/users/views/confirm_email.py
+++ py_backend/users/views/confirm_email.py
@@ -1,25 +1,23 @@
 from django.utils.encoding import force_str
 from django.utils.http import urlsafe_base64_decode
-from django.contrib import messages
 from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth import get_user_model
 
-from users.models import CustomUser
 from users.tokens import account_activation_token
-from django.contrib.auth import login
 
 
+@require_http_methods(["GET"])
 def confirm_email(request, uidb64, token):
+    CustomUser = get_user_model()
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
         user = CustomUser.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
         user = None
+    
     if user is not None and account_activation_token.check_token(user, token):
         user.email_is_verified = True
         user.save()
-        datas = {
-            'uidb64': uidb64,
-            'token': token,
-        }
-        return JsonResponse({'status': "success", 'datas': datas}, status=200)
+        return JsonResponse({'status': "success"}, status=200)
     return JsonResponse({'status': "error"}, status=400)

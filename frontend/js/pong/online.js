62fd1ded7b91f7221385ec3b960966b5f60f3589
gclement42 <clement.games125@gmail.com>
2024-05-21 17:27:18 +0200

    fix lights pos

--- frontend/js/pong/online.js
+++ frontend/js/pong/online.js
@@ -103,7 +103,7 @@ async function connectToLobby(username) {
     console.log("username: ", username);
     if (username == null)
         return ;
-    webSocket = new WebSocket('wss://127.0.0.1:8000/ws/lobby/');
+    webSocket = new WebSocket('ws://127.0.0.1:8080/ws/lobby/');
     
     webSocket.onopen = function() {
         console.log('Connection established');
@@ -174,7 +174,6 @@ async function connectToLobby(username) {
         console.log('Connection closed', e.code, e.reason);
         webSocket = null;
         status.isReady = false;
-        // status.exit = true;
     }
 
     webSocket.onerror = function(e) {
@@ -252,7 +251,10 @@ async function setGameIsStart() {
     if (player && opp && oppInfo) {
         opp.userInfo = oppInfo;
         ClearAllEnv(env);
-        env = await initGame(player, opp);
+        if (player.name == "player1")
+            env = await initGame(player, opp);
+        else
+            env = await initGame(opp, player);
         createHUD(player, opp);
         status.gameIsInit = false;
         status.start = true;

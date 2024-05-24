1789cafc8d61fde2a21e1bde2762ab0be208813b
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-05-24 11:24:32 +0200

    merge with friendList branch

--- frontend/js/Friends.js
+++ frontend/js/Friends.js
@@ -1,6 +1,19 @@
+import { getUserData} from "./User.js"
+
 export function friendsWebsocket() {
     const webSocket = new WebSocket("ws://127.0.0.1:8080/ws/friends/");
     webSocket.onopen = function() {
-        console.log('Connection established');
+        getUserData('username').then((res) => {
+            webSocket.send(JSON.stringify({
+                'type': 'auth',
+                'username': res
+            }));
+        })
+    }
+
+    webSocket.onmessage = (e) => {
+        const data = JSON.parse(e.data);
+        if (data.type == "auth")
+            alert(data.username);
     }
 }
--- py_backend/friends/consumers.py
+++ py_backend/friends/consumers.py
@@ -5,11 +5,13 @@ class FriendsConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         await self.accept()
-        print("CONNECT")
 
     async def disconnect(self, exit_code):
         pass
 
-    async def receive(self, event):
-        print("RECEIVE")
-        pass
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        if text_data_json.get('type') == 'auth':
+            username = text_data_json['username']
+            await self.send(text_data=json.dumps({'type': 'auth', 'username': username }))
+            

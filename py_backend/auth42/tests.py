dc0c7b14ed9d00e14d9ac16c9bf44c6595fc438a
Mathis <maujogue15@gmail.com>
2024-05-27 16:54:51 +0200

    added some tests for auth

--- py_backend/auth42/tests.py
+++ py_backend/auth42/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from unittest.mock import patch
+from django.conf import settings
+from users.models import CustomUser
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class OAuthCallbackTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.oauth_callback_url = reverse('oauth_callback')  # Use the name from urls.py
+
+
+    @patch('requests.post')
+    @patch('requests.get')
+    def test_failure_to_obtain_user_information(self, mock_get, mock_post):
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json.return_value = {'access_token': 'fake_access_token'}
+
+        mock_get.return_value.status_code = 400
+
+        response = self.client.get(self.oauth_callback_url, {'code': 'fake_code'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertIn('/dash?success=false&message=Failed%20to%20obtain%20user%20information%20from%2042%20API.', response.url)
+
+
+    @patch('requests.post')
+    @patch('requests.get')
+    def test_incomplete_user_information(self, mock_get, mock_post):
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json.return_value = {'access_token': 'fake_access_token'}
+        
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {
+            'login': 'testuser',
+            'email': 'testuser@example.com'
+            # Missing 'image' key
+        }
+
+        response = self.client.get(self.oauth_callback_url, {'code': 'fake_code'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertIn('/dash?success=false&message=User%20information%20is%20incomplete%20from%2042%20API.', response.url)
+

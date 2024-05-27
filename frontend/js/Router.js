cfc1578c499839de4be1425eaf5d3aba8f61b35c
Mathis <maujogue15@gmail.com>
2024-05-27 17:47:02 +0200

    dynamic button emailVerified

--- frontend/js/Router.js
+++ frontend/js/Router.js
@@ -25,7 +25,7 @@ const routes = [
 	new Page("Sidebar", "", "html/Sidebar.html", true),
 	new Page("About", "/about", "html/About.html"),
 	new Page("Game", "/game", "html/Game.html", true),
-	new Page("EmailVerified", "/emailVerified", "html/EmailVerified.html"),
+	new Page("EmailVerified", "/emailVerified", "html/EmailVerified.html", true),
 ];
 
 window.addEventListener("popstate", () => router(routes));

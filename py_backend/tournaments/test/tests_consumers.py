c795095bdd052b0b0d928b826161045e07827a8a
Jocelyn Renault <jrenault@z2r2p6.42lyon.fr>
2024-05-29 18:56:58 +0200

    starting testing consumers

---
+++ py_backend/tournaments/test/tests_consumers.py
@@ -0,0 +1,11 @@
+# from channels.testing import WebsocketCommunicator
+# from django.test import TestCase, TransactionTestCase
+# from django.contrib.auth import get_user_model
+# from django.urls import reverse
+# from django.test import Client
+# from users.models import CustomUser
+# from tournaments.models import Tournament, TournamentMatch
+# from myproject.asgi import application
+# from django.core.exceptions import ObjectDoesNotExist
+# import json
+

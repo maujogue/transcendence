b90307c39c0dac705b2c8000e66da57d711f72b1
Mathis <maujogue15@gmail.com>
2024-06-03 21:49:43 +0200

    added cookies to support lang change when unlogged.

--- frontend/js/modules/translationsModule/translationsModule.js
+++ frontend/js/modules/translationsModule/translationsModule.js
@@ -5,6 +5,7 @@ import { getModuleDiv } from "../../Modules.js";
 import { runEndPoint } from "../../ApiUtils.js";
 import { getUserData } from "../../User.js";
 import { initPages } from '../../Router.js';
+import { isLoggedIn } from '../../Utils.js';
 
 
 export async function init() {
@@ -12,40 +13,52 @@ export async function init() {
 	if (!module)
 		return;
 
-	injectTranslations();
 	initLanguageSwitcher();
+	initDropdownListeners();
+	injectTranslations();
 
-	function initLanguageSwitcher() {
+	async function initLanguageSwitcher() {
+		var button = module.querySelector('.languageSwitcher');
+		
+		var lang = await getUserData("lang");
+		if (!lang) {
+			lang = Cookies.get("lang");
+			if (!lang) {
+				lang = navigator.language || navigator.userLanguage;
+				Cookies.set("lang", lang.slice(0, 2));
+			}
+		}
+		var flag = module.querySelector(`[data-lang="${lang}"] > img`).src;
+		var name = module.querySelector(`[data-lang="${lang}"] > span`).textContent.trim();
+		button.querySelector("img").src = flag;
+		button.querySelector(".section-name").textContent = name;
+	}
+
+	async function initDropdownListeners() {
+		var button = module.querySelector('.languageSwitcher');
 		var flags = module.querySelectorAll('.dropdown-item');
+		
 		flags.forEach(flag => {
 			flag.addEventListener('click', async event => {
 				const lang = event.currentTarget.getAttribute('data-lang');
 				const flag = event.currentTarget.querySelector('.flag-icon').src;
 				const name = event.currentTarget.textContent.trim();
-				
-				const button = module.querySelector('.languageSwitcher');
+
 				button.querySelector("img").src = flag;
 				button.querySelector(".section-name").textContent = name;
-				await setLanguage(lang);
+				if(await isLoggedIn())
+					await setLanguage(lang);
+				else
+					Cookies.set("lang", lang);
 				initPages();
 			});
 		});
-		
 	}
 
-	async function setLanguage(userLanguage) {
-		const response = await runEndPoint("users/update_lang/", "POST", JSON.stringify({ lang: userLanguage }));
-		// console.log(response);
-	}
-
-	async function initLanguage() {
-		const userLanguage = navigator.language || navigator.userLanguage;
-		userLanguage = userLanguage.slice(0, 2);
-		setLanguage(userLanguage);
-	}
 	async function injectTranslations() {
-
 		var lang = await getUserData("lang");
+		if (!lang)
+			lang = Cookies.get("lang");
 		console.log(lang);
 		switch (lang) {
 			case "es":
@@ -64,4 +77,8 @@ export async function init() {
 				elm.innerHTML = lang[key];
 		});
 	}
+}
+
+export async function setLanguage(userLanguage) {
+	const response = await runEndPoint("users/update_lang/", "POST", JSON.stringify({ lang: userLanguage }));
 }

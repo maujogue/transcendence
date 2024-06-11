5fddfbe9b1cd24c4cb0ba75d794f0147189bb880
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-06-11 13:07:04 +0200

    add more checks in logout tests

--- py_backend/users/test/test_logout.py
+++ py_backend/users/test/test_logout.py
@@ -1,22 +1,36 @@
 from django.test import TestCase
 from users.models import CustomUser
 from django.urls import reverse
+import json
 
 class LogoutTests(TestCase):
 
     def setUp(self):
-        CustomUser.objects.create_user(
+        self.user = CustomUser.objects.create_user(
             username="lboulatr",
             email="lboulatr@gmail.com",
             password="UserPassword9+")
         
     def test_logout_success(self):
-        self.client.login(username='lboulatr', password='UserPassword9+')
+        user = {
+            'username': 'lboulatr',
+            'password': 'UserPassword9+'}
+
+        self.client.post(
+            reverse('login'),
+             data=json.dumps(user), 
+            content_type='application/json')
+
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.is_online)
+
         response = self.client.post(
             reverse('logout'), 
             content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
+        self.user.refresh_from_db()
+        self.assertFalse(self.user.is_online)
 
     def test_logout_but_not_login(self):
         response = self.client.post(

10371c999c05155894feeea60c60d46cdc248c9a
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-06-11 13:40:33 +0200

    fix tests errors with email_is_verified

--- py_backend/users/test/test_logout.py
+++ py_backend/users/test/test_logout.py
@@ -6,20 +6,26 @@ import json
 class LogoutTests(TestCase):
 
     def setUp(self):
+        self.assertEqual(CustomUser.objects.count(), 0)
         self.user = CustomUser.objects.create_user(
             username="lboulatr",
             email="lboulatr@gmail.com",
             password="UserPassword9+")
+        self.assertEqual(CustomUser.objects.count(), 1)
+        self.user.email_is_verified = True
+        self.user.save()
+        self.user.refresh_from_db()
         
     def test_logout_success(self):
         user = {
             'username': 'lboulatr',
             'password': 'UserPassword9+'}
 
-        self.client.post(
+        response = self.client.post(
             reverse('login'),
              data=json.dumps(user), 
             content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
         self.user.refresh_from_db()
         self.assertTrue(self.user.is_online)

d1d90e26efd329741e323bd761a30b36db586243
Jocelyn Renault <jrenault@z2r3p7.42lyon.fr>
2024-06-12 16:32:45 +0200

    more things in the contract deployement

--- blockchain/tournamentContract.py
+++ blockchain/tournamentContract.py
@@ -5,9 +5,27 @@ def deployContract(newMessage):
     with open("compiledSolidity.json", "r") as file:
         compiledSolidity = json.load(file)
 
-    provider_url = "https://sepolia.infura.io/v3/2dc50a5c22934a019f88758e28ba4bfc"
+    provider_url = "https://sepolia.infura.io/v3/098a45a55c344ef8ac3da0ba6270fd1f"
     w3 = Web3(HTTPProvider(provider_url))
 
+    abi = compiledSolidity["contracts"]["tournamentContract.sol"]["StoreTournamentsScore"]["abi"]
+    bytecode = compiledSolidity["contracts"]["tournamentContract.sol"]["StoreTournamentScore"]["evm"]["bytecode"]["object"]
     
-    public_key = '0x08Cad489Ad4542AF82Ee81eC949776775dea23be'
+    contract = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+    chainId = 11155111
+    wallet = '0x08Cad489Ad4542AF82Ee81eC949776775dea23be'
     private_key = 'c38331b12e562919c0d636d90d526accf9e1b442f31cb6cd6254e1c231b97859'
+    nonce = w3.eth.get_transaction_count(wallet)
+
+    transaction = contract.constructor().build_transaction({
+        'gasPrice': w3.eth.gas_price,
+        'chainId': chainId,
+        'from': wallet,
+        'nonce': nonce
+    })
+
+    signedTransaction = w3.eth.account.sign_transaction(transaction, private_key)
+    transactionHash = w3.eth.send_raw_transaction(signedTransaction.rawTransaction)
+    transactionReceipt = w3.eth.wait_for_transaction_receipt(transactionHash)
+    contractAddress = transactionReceipt.contractAddress

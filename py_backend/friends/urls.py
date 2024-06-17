aa26fc2d6125e04953d7c1b161f4964c5b935905
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-06-17 10:58:54 +0200

    remove send_request url

--- py_backend/friends/urls.py
+++ py_backend/friends/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from friends import views
 
 urlpatterns = [
-	path('send_request/<str:username>/', views.send_request, name='send_request'),
 	path('remove/<str:friend_username>/', views.remove, name='remove'),
 ]

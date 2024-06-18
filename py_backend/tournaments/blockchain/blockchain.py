c78529e9b9877e4cb8639026754b6d7004031cdf
jrenault <jrenault@student.42lyon.fr>
2024-06-18 11:20:55 +0200

    getter done

--- py_backend/tournaments/blockchain/blockchain.py
+++ py_backend/tournaments/blockchain/blockchain.py
@@ -50,22 +50,22 @@ def set_data_on_blockchain(tournament):
     transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
     print("Done! Matches and winner set.")
 
-def get_matches_by_player(player_name):
+def get_matches_by_player(tournament, player_name):
     contract_address = tournament.contract_address
-    tournament_winner = tournament.get_winner()
 
     abi = load_contract_abi()
     w3 = Web3(HTTPProvider(settings.PROVIDER_URL))
     contract = w3.eth.contract(address=contract_address, abi=abi)
-    nonce = w3.eth.get_transaction_count(settings.WALLET)
-    transaction = contract.functions.getMatches().build_transaction({
-        'gasPrice': w3.eth.gas_price,
-        'chainId': settings.CHAIN_ID,
-        'from': settings.WALLET,
-        'nonce': nonce
-    })
-    signed_transaction = w3.eth.account.sign_transaction(transaction, settings.PRIVATE_KEY)
-    transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
-    print("Waiting for transaction to finish...")
-    transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
-    print("Done! Matches and winner set.")
+    matches = contract.functions.getMatches().call()
+
+    player_matches = [{
+            'round': match[1],
+            'player1': match[2],
+            'score_player_1': match[3],
+            'player2': match[4],
+            'score_player_2': match[5],
+            'match_winner': match[6],
+        }
+        for match in matches if match[2] == player_name or match[4] == player_name
+    ]
+    return player_matches

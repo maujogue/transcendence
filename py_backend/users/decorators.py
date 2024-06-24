3d96cb3fad2aec77469c89a551d69e1eafcb87cd
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-06-24 10:40:29 +0200

    add decorators.py and is_online field in users/models.py

---
+++ py_backend/users/decorators.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+
+def custom_login_required(func):
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_online:
+            return func(request, *args, **kwargs)
+        if not request.user.is_authenticated:
+            return JsonResponse({'error': 'User is not authenticated'}, status=401)
+        return func(request, *args, **kwargs)
+    return wrapper

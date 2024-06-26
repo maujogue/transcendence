927dc234e06604a340c08540f5010501cb7a007e
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-06-26 16:00:31 +0200

    fix get_user_data 404 issue and clean the new decorator

--- py_backend/users/decorators.py
+++ py_backend/users/decorators.py
@@ -1,15 +1,10 @@
 from django.http import JsonResponse
 from django.contrib.auth.models import AnonymousUser
+from django.shortcuts import redirect
 
 def custom_login_required(func):
     def wrapper(request, *args, **kwargs):
-        if isinstance(request.user, AnonymousUser):
-            return JsonResponse({'error': 'User does not exist'}, status=404)
-        if not request.user:
-            return JsonResponse({'error': 'There is no user in the request'}, status=404)
-        if not request.user.email_is_verified:
-            return JsonResponse({'error': 'Email is not verified'}, status=404)
-        if not request.user.is_online:
-            return JsonResponse({'error': 'User is not connected'}, status=404)
+        if isinstance(request.user, AnonymousUser) or not request.user or not request.user.email_is_verified or not request.user.is_online:
+            return redirect('/dash')
         return func(request, *args, **kwargs)
     return wrapper

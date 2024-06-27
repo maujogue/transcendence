fa2400fe65cf5054c5bf276559da15e199d082a1
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-06-27 15:50:59 +0200

    remove useless console log

--- frontend/js/User.js
+++ frontend/js/User.js
@@ -5,7 +5,6 @@ async function injectUserData(divToFill = null, username = null) {
 	if (await isLoggedIn())
 	{
 		var userInfos = await getUserData(null, username);
-    console.log(userInfos);
 		if(!divToFill)
 			divToFill = document;
 		Object.keys(userInfos).forEach((info) => {

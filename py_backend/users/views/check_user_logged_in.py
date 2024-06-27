8d255cc30740b3e57dc2d5de5a49bf26fefa019b
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-06-27 13:35:02 +0200

    rework of check_user_logged_in function

--- py_backend/users/views/check_user_logged_in.py
+++ py_backend/users/views/check_user_logged_in.py
@@ -1,12 +1,17 @@
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import requires_csrf_token
 from django.http import JsonResponse
-from django.contrib.auth import get_user_model
-from django.contrib.sessions.models import Session
-from django.utils import timezone
-import logging
+from django.contrib.auth.models import AnonymousUser
+
+from users.models import CustomUser
 
 @require_http_methods(["GET"])
 @requires_csrf_token
 def check_user_logged_in_view(request):
-    return JsonResponse({'is_logged_in': request.user.is_online}, status=200)
+    if isinstance(request.user, AnonymousUser) or not request.user:
+        return JsonResponse({'error': 'AnonymousUser'}, status=400)
+    try:
+        user = CustomUser.objects.get(username=request.user.username)
+    except CustomUser.DoesNotExist:
+        return JsonResponse({'error': 'User not found'}, status=400)
+    return JsonResponse({'is_logged_in': user.is_online}, status=200)

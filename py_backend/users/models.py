36955d0baceb906ae9f2d21ac0094a36e5b5487b
Mathis <maujogue15@gmail.com>
2024-07-08 11:21:27 +0200

    fixed textarea

--- py_backend/users/models.py
+++ py_backend/users/models.py
@@ -25,7 +25,7 @@ class CustomUser(AbstractUser):
 	username = models.CharField(max_length=settings.MAX_LEN_USERNAME, unique=True)
 	tournament_username = models.CharField(max_length=settings.MAX_LEN_USERNAME, unique=True)
 	email = models.EmailField(max_length=settings.MAX_LEN_EMAIL, unique=True)
-	email_is_verified = models.BooleanField(default=False)
+	email_is_verified = models.BooleanField(default=True)
 	title = models.CharField(max_length=50, null=True)
 	avatar = models.ImageField(default='avatar.jpg', upload_to='profile_avatars')
 	bio = models.TextField(max_length=settings.MAX_LEN_TEXT, default="")

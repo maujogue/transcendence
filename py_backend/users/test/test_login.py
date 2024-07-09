729ff4a60eb677b9a360486d44be75fff11397b4
gclement42 <clement.games125@gmail.com>
2024-07-09 16:04:40 +0200

    update tests

--- py_backend/users/test/test_login.py
+++ py_backend/users/test/test_login.py
@@ -9,7 +9,7 @@ class LoginTests(TestCase):
         self.user = CustomUser.objects.create_user(
             username="lboulatr",
             email="lboulatr@gmail.com",
-            password="Damiendubocal75")
+            password="Mewtransse9+")
         self.user.email_is_verified = True
         self.user.save()
 
@@ -23,7 +23,7 @@ class LoginTests(TestCase):
     def test_basic_login(self):
         user = {
             'username': 'lboulatr',
-            'password': 'Damiendubocal75'
+            'password': 'Mewtransse9+'
         }
         self.assertFalse(self.user.is_online)
 
@@ -56,7 +56,7 @@ class LoginTests(TestCase):
     def test_not_json_login(self):
         user = {
             'username': 'lboulatr',
-            'password': 'Damiendubocal75'
+            'password': 'Mewtransse9+'
         }
 
         response = self.client.post(
@@ -68,7 +68,7 @@ class LoginTests(TestCase):
     def test_wrong_login(self):
         user = {
             'username': 'lboulatx',
-            'password': 'Damiendubocal75'
+            'password': 'Mewtransse9+'
         }
 
         response = self.client.post(
@@ -81,7 +81,7 @@ class LoginTests(TestCase):
     def test_wrong_password(self):
         user = {
             'username': 'lboulatr',
-            'password': 'Damiendubocal75*'
+            'password': 'Mewtransse9+*'
         }
 
         response = self.client.post(
@@ -94,7 +94,7 @@ class LoginTests(TestCase):
     def test_missing_username(self):
         user = {
             'username': '',
-            'password': 'Damiendubocal75'
+            'password': 'Mewtransse9+'
         }
 
         response = self.client.post(
@@ -130,7 +130,7 @@ class LoginTests(TestCase):
     def test_login_invalid_credentials(self):
         user = {
             'username': 'lboulatr',
-            'password': 'Damiendubocal75+'
+            'password': 'Mewtransse9++'
         }
 
         response = self.client.post(
@@ -148,7 +148,7 @@ class LoginTests(TestCase):
 
         user = {
             'username': 'lboulatr',
-            'password': 'Damiendubocal75'
+            'password': 'Mewtransse9+'
         }
 
         response = self.client.post(
@@ -159,4 +159,82 @@ class LoginTests(TestCase):
         self.assertEqual(response.status_code, 400)
         response_data = response.json()
         self.assertEqual(response_data.get('error'), "Your email is not verified yet.")
-    
+
+
+class SessionKeyTests(TestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username="lboulatr",
+            email="lboulatr@gmail.com",
+            password="Mewtransse9+")
+        self.user.email_is_verified = True
+        self.user.save()
+
+    def test_session_key_is_equal_to_request_session_key(self):
+        user = {
+            'username': 'lboulatr',
+            'password': 'Mewtransse9+'
+        }
+        self.assertFalse(self.user.is_online)
+
+        response = self.client.post(
+            reverse('login'), 
+            data=json.dumps(user), 
+            content_type='application/json')
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('_auth_user_id' in self.client.session)   
+        self.user.refresh_from_db()
+
+        self.assertEqual(self.user.session_key, self.client.session.session_key)
+
+    def test_session_key_is_deleted_when_logout(self):
+        user = {
+            'username': 'lboulatr',
+            'password': 'Mewtransse9+'
+        }
+                
+        self.client.post(
+            reverse('login'), 
+            data=json.dumps(user), 
+            content_type='application/json')
+        self.user.refresh_from_db()
+        first_session_key = self.user.session_key
+        self.assertEqual(self.user.session_key, self.client.session.session_key)
+
+        self.client.post(
+            reverse('logout'), 
+            data=json.dumps(user), 
+            content_type='application/json')
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.session_key, None)
+        
+        self.client.post(
+            reverse('login'), 
+            data=json.dumps(user), 
+            content_type='application/json')
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.session_key, self.client.session.session_key)
+        self.assertNotEqual(self.user.session_key, first_session_key)
+
+    def test_already_logged_in(self):
+        user = {
+            'username': 'lboulatr',
+            'password': 'Mewtransse9+'
+        }
+
+        self.client.post(
+            reverse('login'), 
+            data=json.dumps(user), 
+            content_type='application/json')
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.session_key, self.client.session.session_key)
+
+        response = self.client.post(
+            reverse('login'), 
+            data=json.dumps(user), 
+            content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        response_data = response.json()
+        self.assertEqual(response_data.get('error'), "You are already logged in somewhere else.")

060816403bff98db0873b844be1f663b2260d4f5
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-07-10 14:05:28 +0200

    check for update_lang

--- py_backend/users/views/update_lang.py
+++ py_backend/users/views/update_lang.py
@@ -4,6 +4,7 @@ from users.decorators import custom_login_required as login_required
 from django.http import JsonResponse
 from users.utils import decode_json_body, lang_is_valid
 
+
 @require_http_methods(['POST'])
 @requires_csrf_token
 @login_required
@@ -22,7 +23,7 @@ def update_lang(request):
             user = request.user
             user.lang = lang
             user.save()
-            return JsonResponse({'status': 'success', 'message': 'Language updated successfully', 'lang': lang})
+            return JsonResponse({'status': 'success', 'message': 'Language updated successfully'})
         else:
             return JsonResponse({'status': 'error', 'message': 'Invalid language or user not authenticated'}, status=400)
 

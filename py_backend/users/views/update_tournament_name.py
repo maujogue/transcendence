5739c10b1fbbdbb1b38ffda38bfbfa5cc439df14
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-07-11 15:44:39 +0200

    remove print in test

--- py_backend/users/views/update_tournament_name.py
+++ py_backend/users/views/update_tournament_name.py
@@ -18,7 +18,6 @@ def update_tournament_name(request):
     tournament_username = data.get('username')
     is_valid, is_valid_error = username_is_valid(tournament_username)
     if not is_valid:
-        print('not valid =', tournament_username)
         return JsonResponse({'error': is_valid_error}, status=400)
 
     if tournament_username:

e0e01413dc5e3209d991cce3c56d16c12982f879
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-07-15 16:58:49 +0200

    remove time in tasks.py

--- py_backend/users/tasks.py
+++ py_backend/users/tasks.py
@@ -1,9 +1,4 @@
-from datetime import timedelta
-from django.utils import timezone
 from .models import CustomUser
 
 def check_unverified_accounts():
-    now = timezone.now()
-    three_minutes_ago = now - timedelta(minutes=3)
-    
-    CustomUser.objects.filter(account_creation_time__lte=three_minutes_ago).delete()
+    CustomUser.objects.filter(email_is_verified=False).delete()

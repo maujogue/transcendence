d6efbc3e2888d562cd6002c6c8cfb802ad7a1f64
LezardSC <117669219+LezardSC@users.noreply.github.com>
2024-07-16 21:07:07 +0200

    removed private key and provider url

--- py_backend/tournaments/blockchain.py
+++ py_backend/tournaments/blockchain.py
@@ -1,20 +1,16 @@
 from web3 import Web3, HTTPProvider
 import os
 import json
-from dotenv import load_dotenv
 # import logging
 # from asgiref.sync import sync_to_async
 
 # logger = logging.getLogger(__name__)
 
+CHAIN_ID = 11155111 #TODO find if it's working with the env in prod, if not find a solution
+WALLET = os.environ.get("WALLET")
+PRIVATE_KEY = os.environ.get("PRIVATE_KEY")
+PROVIDER_URL = os.environ.get("PROVIDER_URL")
 CONTRACT_ADDRESS = os.environ.get("CONTRACT_ADDRESS")
-
-load_dotenv(".env.tournament")
-
-CHAIN_ID = 11155111 #Sepolia chain ID
-WALLET = os.getenv("WALLET")
-PRIVATE_KEY = os.getenv("PRIVATE_KEY")
-PROVIDER_URL = os.getenv("PROVIDER_URL")
 
 def load_abi_from_file(json_path):
     with open(json_path, "r") as file:
@@ -63,7 +59,7 @@ def set_data_on_blockchain(tournament):
         signed_transaction = w3.eth.account.sign_transaction(transaction, PRIVATE_KEY)
         transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
         print("Waiting for transaction to finish...")
-        transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash, timeout=600)
+        transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
         print("Done! Matches and winner set.")
         return transaction_receipt
     except Exception as e:

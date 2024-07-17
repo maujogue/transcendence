693896b3125c2ee982738496074752a9913d0edb
gclement42 <clement.games125@gmail.com>
2024-07-17 13:53:03 +0200

    fix bug with tournament end

--- py_backend/tournaments/consumers.py
+++ py_backend/tournaments/consumers.py
@@ -141,20 +141,20 @@ class TournamentConsumer(AsyncWebsocketConsumer):
         
     async def create_history_match(self, lobby):
         try:
-            self.match = await Match.objects.aget(lobby_id=str(lobby.uuid))
-            return self.match
+            match = await Match.objects.aget(lobby_id=str(lobby.uuid))
+            return match
         except Match.DoesNotExist:
-            player1 = await self.authenticate_user_with_id(self.match.player1)
-            player2 = await self.authenticate_user_with_id(self.match.player2)
+            player1 = await self.authenticate_user_with_username(self.match.player1)
+            player2 = await self.authenticate_user_with_username(self.match.player2)
             winner = player1 if lobby.player1 else player2
             loser = player2 if winner == player1 else player1
             match = Match(  lobby_id=str(lobby.uuid),
-                            player1=player1, 
-                            player2=player2, 
+                            player1=player1.id, 
+                            player2=player2.id, 
                             player1_average_exchange=0,
                             player2_average_exchange=0,
-                            winner=winner,
-                            loser=loser,
+                            winner=winner.id,
+                            loser=loser.id,
                             player1_score=0,
                             player2_score=0)
             await match.asave()
@@ -165,9 +165,9 @@ class TournamentConsumer(AsyncWebsocketConsumer):
         await match.asave()
         print(f'match.player1 = {match.player1}, match.player2 = {match.player2}, self.scope[].username {self.scope["user"].username}')
         if match.player1 == self.scope['user'].username:
-            await asyncio.sleep(30)
+            await asyncio.sleep(10)
         if match.player2 == self.scope['user'].username:
-            await asyncio.sleep(32)
+            await asyncio.sleep(12)
 
     async def cancel_match(self, match):
         await self.send_data({'type': 'status', 'status': 'cancelled', 'message': f'Match not started in time, {match.winner} wins!'})
@@ -221,7 +221,6 @@ class TournamentConsumer(AsyncWebsocketConsumer):
 
     async def set_match_info(self):
         try:
-            print('here')
             match = await self.get_match_result()
             player1 = await self.authenticate_user_with_id(match.player1)
             player2 = await self.authenticate_user_with_id(match.player2)
@@ -402,8 +401,6 @@ class TournamentConsumer(AsyncWebsocketConsumer):
         )
 
     async def send_tournament_ranking(self):
-        if self.tournament.finished is False:
-            return
         winner = await sync_to_async(self.tournament.get_winner)()
         ranking = await sync_to_async(self.tournament.get_ranking)()
         await self.send_data({'type': 'ranking', 'winner': winner, 'ranking': ranking})

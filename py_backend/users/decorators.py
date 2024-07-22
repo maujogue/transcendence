221cb3b0ebe027271e9e56216987b026080d4abb
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-07-22 16:20:52 +0200

    add is_in_game decorator

--- py_backend/users/decorators.py
+++ py_backend/users/decorators.py
@@ -7,3 +7,10 @@ def custom_login_required(func):
             return JsonResponse({'status': 'error', 'message': 'You need to be logged in to access this page'}, status=401)
         return func(request, *args, **kwargs)
     return wrapper
+
+def is_in_game(func):
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_ingame:
+            return JsonResponse({'status': 'error', 'message': 'You cannot change your infos while you are in a game'}, status=400)
+        return func(request, *args, **kwargs)
+    return wrapper

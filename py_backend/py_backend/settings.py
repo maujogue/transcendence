d358910919d3afb9743d947a3d09626ee6022517
Mathis <maujogue15@gmail.com>
2024-07-22 13:34:01 +0200

    fix 42_auth env value

--- py_backend/py_backend/settings.py
+++ py_backend/py_backend/settings.py
@@ -12,11 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
-import os
 import environ
 
-
 env = environ.Env()
+environ.Env.read_env()
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
@@ -28,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
+SECRET_KEY = env("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)

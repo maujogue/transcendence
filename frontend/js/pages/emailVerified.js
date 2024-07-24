81b6f3b9732fe4382240d50158d6240053f20a67
Mathis <maujogue15@gmail.com>
2024-07-24 11:07:42 +0200

    added back router for about page

--- frontend/js/pages/emailVerified.js
+++ frontend/js/pages/emailVerified.js
@@ -5,7 +5,7 @@ import { isLoggedIn } from "../Utils.js";
 export async function init(queryParams) {
 	var redirectButon = document.getElementById('redirectButton');
 	if (await isLoggedIn())
-		redirectButon.innerHTML = '<button href="/dash" class="btn btn-success"><span data-lang="go_dash"> Go to dashboard</span></button>';
+		redirectButon.innerHTML = '<button href="/dash" class="btn btn-success" [navlink]><span data-lang="go_dash"> Go to dashboard</span></button>';
 	else
 		redirectButon.innerHTML = '<button data-bs-toggle="modal" data-bs-target="#login" class="btn btn-success"><span data-lang="go_login_page">Go to login page</span></button>';
 	injectElementTranslations("#emailVerified");
